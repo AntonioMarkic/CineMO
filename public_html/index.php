e4a154e5ca236918b6c96e844d6b09ed25cee726
AntonioMarkic <44848235+AntonioMarkic@users.noreply.github.com>
2020-02-03 12:05:36 +0100

    Add files via upload

---
+++ public_html/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/styles.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title>CineMo</title>
+    <link rel="icon" type="image/png" href="img/logo.png">
+</head>
+
+<body>
+    <?php
+    $link = mysqli_connect("localhost", "id12090993_cinema", "cinema", "id12090993_cinema_db");
+    $sql = "SELECT * FROM movieTable";
+    ?>
+    <header></header>
+    <div id="home-section-1" class="movie-show-container">
+        <h1>Trenutno prikazujemo</h1>
+        <h3>Rezervirajte kartu odmah</h3>
+
+        <div class="movies-container">
+
+            <?php
+                        if($result = mysqli_query($link, $sql)){
+                            if(mysqli_num_rows($result) > 0){
+                                for ($i = 0; $i <= 5; $i++){
+                                    $row = mysqli_fetch_array($result);
+                                    echo '<div class="movie-box">';
+                                    echo '<img src="'. $row['movieImg'] .'" alt=" ">';
+                                    echo '<div class="movie-info ">';
+                                    echo '<h3>'. $row['movieTitle'] .'</h3>';
+                                    echo '<a href="booking.php?id='.$row['movieID'].'"><i class="fas fa-ticket-alt"></i> Rezervirajte kartu</a>';
+                                    echo '</div>';
+                                    echo '</div>';
+                                }
+                                mysqli_free_result($result);
+                            } else{
+                                echo '<h4 class="no-annot">Trentuno ne prikazujemo</h4>';
+                            }
+                        } else{
+                            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                        }
+                        
+                        // Close connection
+                        mysqli_close($link);
+                        ?>
+        </div>
+    </div>
+    <div id="home-section-2" class="services-section">
+        <h1>Kako sustav funkcionira</h1>
+
+        <div class="services-container">
+            <div class="service-item">
+                <div class="service-item-icon">
+                    <i class="fas fa-4x fa-video"></i>
+                </div>
+                <h2>1. Odaberite film </h2>
+                <p>Odaberite film koji želite gledati u našem kinu</p>
+            </div>
+            <div class="service-item">
+                <div class="service-item-icon">
+                     <i class="fas fa-4x fa-theater-masks"></i>
+                </div>
+                <h2>2. Odaberite sjedalo </h2>
+                <p>Odaberite sjedalo i popunite potrebne podatke</p>
+            </div>
+            <div class="service-item">
+                <div class="service-item-icon">
+                    <i class="fas fa-4x fa-theater-masks"></i>
+                </div>
+                <h2>3. Rezervirajte kartu</h2>
+                <p>Odabirom gumba rezerviraj potvrdili ste svoju rezervaciju</p>
+            </div>
+            <div class="service-item"></div>
+            <div class="service-item"></div>
+        </div>
+    </div>
+    <div id="home-section-3" class="trailers-section">
+        <h1 class="section-title">Pretražite nove filmove</h1>
+        <h3>Upravo prikazujemo</h3>
+        <div class="trailers-grid">
+            <div class="trailers-grid-item">
+                <img src="img/movie-thumb-1.jpg" alt="">
+                <div class="trailer-item-info" data-video="Z1BCujX3pw8">
+                    <h3>Captain Marvel</h3>
+                    <i class="far fa-3x fa-play-circle"></i>
+                </div>
+            </div>
+            <div class="trailers-grid-item">
+                <img src="img/movie-thumb-2.jpg" alt="">
+                <div class="trailer-item-info" data-video="p9wE8dyzEJE">
+                    <h3>A Quiet Place</h3>
+                    <i class="far fa-3x fa-play-circle"></i>
+                </div>
+            </div>
+            <div class="trailers-grid-item">
+                <img src="img/movie-thumb-3.jpg" alt="">
+                <div class="trailer-item-info" data-video="cksYkEzUa7k">
+                    <h3>The Lego Movie</h3>
+                    <i class="far fa-3x fa-play-circle"></i>
+                </div>
+            </div>
+            <div class="trailers-grid-item">
+                <img src="img/movie-thumb-4.jpg" alt="">
+                <div class="trailer-item-info" data-video="Ze5YA4mkzhI">
+                    <h3>Secret Men Club</h3>
+                    <i class="far fa-3x fa-play-circle"></i>
+                </div>
+            </div>
+            <div class="trailers-grid-item">
+                <img src="img/movie-thumb-5.jpg" alt="">
+                <div class="trailer-item-info" data-video="lbUAVTc7NTo">
+                    <h3>Vice</h3>
+                    <i class="far fa-3x fa-play-circle"></i>
+                </div>
+            </div>
+            <div class="trailers-grid-item">
+                <img src="img/movie-thumb-6.jpg" alt="">
+                <div class="trailer-item-info" data-video="RyFlfN4dD14">
+                    <h3>The Vanishing</h3>
+                    <i class="far fa-3x fa-play-circle"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer></footer>
+
+    <script src="scripts/jquery-3.3.1.min.js "></script>
+    <script src="scripts/script.js "></script>
+</body>
+
+</html>
